9f0c3367377f3e44770fad4b386db4e09dcdd37e
graeme_laptop <graemeborland@gmail.com>
2017-05-16 20:36:10 -0500

    adding some basic hand scripts that spawn projectiles

---
+++ Assets/Scripts/HandShipController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class HandShipController : MonoBehaviour
+{
+
+	[SerializeField]
+	private GameObject projectileToSpawn;
+	[SerializeField]
+	private Transform spawnLocation;
+
+	private SteamVR_TrackedObject controller;
+
+	private void Awake()
+	{
+		controller = GetComponent<SteamVR_TrackedObject>();
+	}
+
+	private void Update()
+	{
+		var device = SteamVR_Controller.Input((int)controller.index);
+		
+		if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+		{
+			GameObject projectile = Instantiate(projectileToSpawn);
+			projectile.transform.position = spawnLocation.position;
+			projectile.transform.rotation = spawnLocation.rotation;
+		}
+	}
+
+}
