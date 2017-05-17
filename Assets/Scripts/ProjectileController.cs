10cf9253b0725c78d2cd5bb9e0de7e0c18b68ec1
graeme_laptop <graemeborland@gmail.com>
2017-05-16 22:51:54 -0500

    fixing merge conflicts

--- Assets/Scripts/ProjectileController.cs
+++ Assets/Scripts/ProjectileController.cs
@@ -9,11 +9,26 @@ public class ProjectileController : MonoBehaviour
 	private Rigidbody rb;
 	[SerializeField]
 	private float launchForceAmount = 10f;
-
+	[SerializeField]
+	private bool isVRPlayer = true;
+	public bool IsVRPlayer { get { return isVRPlayer; } }
 
 	public void Launch()
 	{
 		rb.AddForce(transform.forward * launchForceAmount);
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		DamageableObject dmg = collision.collider.gameObject.GetComponent<DamageableObject>();
+		if (dmg != null)
+		{
+			if (dmg.IsVRPlayer != isVRPlayer)
+			{
+				dmg.GetHit(this);
+				Destroy(this.gameObject);
+			}
+		}
 	}
 
 }
