cfa8a8cb284adc1735a9ff079b51906364c29851
KAIS-DELL-LAPTOP\Kai <kai@kaiclavier.com>
2017-05-17 21:06:59 -0500

    Updated tank scene, prefab, and script.

--- Assets/Scripts/Tank_Basic.cs
+++ Assets/Scripts/Tank_Basic.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tank_Basic : MonoBehaviour {
+  [Range(0,1)] 
+  public int tankIndex = 0;
 
-  // not the best way but ok for now. if false we're player 1, if true player two. 
-  public bool isTank2 = false;
+  private string _tankIndex{
+    get{
+      return (tankIndex + 1).ToString();
+    }
+  }
 
   // Input
-  public Vector2 inputDirection = Vector2.zero;
-  public bool isShootingActive = false;
-  public bool isShieldingActive = false;
+  public Vector3 input;
+  public float inputMag;
+  public float inputSensitivity = 1f;
+
+  public Transform pivot;
+  public Vector3 look;
+  public Vector3 move;
+  public float moveSpeed = 8f;
+  public float turnSpeed = 8f;
+  public float accelSpeed = 5f;
+  public float decelSpeed = 8f;
 
 
 	// Use this for initialization
@@ -18,32 +31,29 @@ public class Tank_Basic : MonoBehaviour {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
-    GetInput();
+      input = new Vector3(Input.GetAxisRaw("Horizontal_Tank" + _tankIndex), 0f, Input.GetAxisRaw("Vertical_Tank" + _tankIndex)) * inputSensitivity;
+      inputMag = Mathf.Clamp01(input.magnitude);
+      if(inputMag > 0f){
+        look = RoundToCardinal(input.normalized).normalized;
+      }
 
-    isShootingActive = Input.GetButton("Fire1_Tank1");
-    Debug.Log(Input.GetKey(KeyCode.A) + " " + Input.GetKey(KeyCode.LeftArrow));
-   
+      //float myInputAngle = Mathf.Atan2(input.x, input.z) * Mathf.Rad2Deg;
+      pivot.localEulerAngles = new Vector3(0,Mathf.LerpAngle(pivot.localEulerAngles.y, Mathf.Atan2(look.x, look.z) * Mathf.Rad2Deg, turnSpeed * Time.deltaTime),0);
+
+      float myLerpSpeed = inputMag == 0f ? decelSpeed : accelSpeed; //how fast to lerp
+      move = Vector3.Lerp(move,RoundToCardinal(input.normalized).normalized * moveSpeed * inputMag,Time.deltaTime * myLerpSpeed);
+
+      transform.position += move * Time.deltaTime;
+
+      if(Input.GetButtonDown("Fire1_Tank" + _tankIndex)){
+        Debug.Log("Pew pew I am tank " + _tankIndex);
+      }
+      if(Input.GetButton("Fire2_Tank" + _tankIndex)){
+        Debug.Log("I am shielding........ also I am tank " + _tankIndex);
+      }
 	}
-
-  private void GetInput(){
-
-    if(!isTank2){
-      // Tank 1
-
-      isShootingActive = Input.GetButton("Fire1_Tank1");
-      isShieldingActive = Input.GetButton("Fire2_Tank1");
-      inputDirection.x = Input.GetAxis("Horizontal_Tank1");
-      inputDirection.y = Input.GetAxis("Vertical_Tank1");
-
-    }else{
-      // is tank 2
-      isShootingActive = Input.GetButton("Fire1_Tank2");
-      isShieldingActive = Input.GetButton("Fire2_Tank2");
-      inputDirection.x = Input.GetAxis("Horizontal_Tank2");
-      inputDirection.y = Input.GetAxis("Vertical_Tank2");
-    }
+  public Vector3 RoundToCardinal(Vector3 myVec){
+    return new Vector3(Mathf.Round(myVec.x),Mathf.Round(myVec.y),Mathf.Round(myVec.z));
   }
 }
