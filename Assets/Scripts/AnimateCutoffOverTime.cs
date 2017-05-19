021c1bdf13774fe1d415f6d4312811afdae249ab
KAIS-DELL-LAPTOP\Kai <kai@kaiclavier.com>
2017-05-18 20:10:03 -0500

    shield animates over lifetime, now

---
+++ Assets/Scripts/AnimateCutoffOverTime.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateCutoffOverTime : MonoBehaviour {
+	public Renderer r;
+	public float startingCutoff = 0.6f;
+	public float time = 3f;
+	private float timer;
+	private float range;
+	// Use this for initialization
+	void Start () {
+		range = 1f - startingCutoff;
+	}
+	void Update () {
+		float newCutoff = ((timer / time) * range) + startingCutoff;
+		newCutoff = Mathf.Clamp01(newCutoff);
+		Debug.Log(newCutoff);
+		r.material.SetFloat("_Cutoff", newCutoff);
+
+		timer += Time.deltaTime;
+	}
+}
